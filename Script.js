a191e301b995f9d2e3fde2cccf4efcafc09a3203
Bappy71 <19103076@iubat.edu>
2022-09-01 10:36:57 +0600

    2nd Commit

---
+++ Script.js
@@ -0,0 +1,4 @@
+let newTask = document.querySelector('#new-task');
+let form = document.querySelector('form');
+let todoUl = document.querySelector('#items');
+let completeUl = document.querySelector('.complete-list ul');
