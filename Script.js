bbe4e8e6df27d2c775b5eed607ff614578d7cfbd
Bappy71 <19103076@iubat.edu>
2022-09-02 22:09:18 +0600

    3rd commit

--- Script.js
+++ Script.js
@@ -2,3 +2,18 @@ let newTask = document.querySelector('#new-task');
 let form = document.querySelector('form');
 let todoUl = document.querySelector('#items');
 let completeUl = document.querySelector('.complete-list ul');
+
+
+let createTask = function (task) {
+    let listItem = document.createElement('li');
+    let checkBox = document.createElement('input');
+    let label = document.createElement('label');
+
+    label.innerText = task;
+    checkBox.type = 'checkbox';
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+
+    return listItem;
+}
